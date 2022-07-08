41425bd2b5f6bcd2c51b06cc14d0969358485c54
myeshagrant <65326269+myeshagrant@users.noreply.github.com>
2022-07-08 00:44:42 -0400

    Expense Project
    Creating an expense reimbursement system.

---
+++ RevEmployee.java
@@ -0,0 +1,35 @@
+package com.Reimbursement;
+
+public class RevEmployee {
+//define Strings
+	private String username;
+	private String password;
+	private String revEmployeeRole;
+	
+//Setting and getting strings
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getRevEmployeeRole() {
+		return revEmployeeRole;
+	}
+	public void setRevEmployeeRole(String revEmployeeRole) {
+		this.revEmployeeRole = revEmployeeRole;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
